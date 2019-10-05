fb995718a154754f7717d578324254c771b07612
Omar Zabaneh <zabano@gmail.com>
2019-10-04 20:58:05 -0700

    improved date and time parsing

--- jsonapi/datatypes.py
+++ jsonapi/datatypes.py
@@ -7,21 +7,20 @@ from sqlalchemy.sql import sqltypes
 from jsonapi.db.filter import FilterClause, Operator
 from jsonapi.exc import DataTypeError
 
+accept_date = ('%Y-%m-%d', '%Y-%m')
+accept_time = ('%H:%M:%S', '%H:%M', '%I:%M%p', '%I:%M %p')
+accept_datetime = ('%Y-%m-%dT%H:%M:%SZ',
+                   '%Y-%m-%dT%H:%M:%S', '%Y-%m-%d %H:%M:%S', '%Y-%m-%d %I:%M:%S%p', '%Y-%m-%d %I:%M:%S %p',
+                   '%Y-%m-%dT%H:%M', '%Y-%m-%d %H:%M', '%Y-%m-%d %I:%M', '%Y-%m-%d %I:%M%p', '%Y-%m-%d %I:%M %p')
+
 
 class DataType:
     FORMAT_DATETIME = '%Y-%m-%dT%H:%M:%SZ'
     FORMAT_DATE = '%Y-%m-%d'
     FORMAT_TIME = '%H:%M:%S'
-    DATETIME_ACCEPT = (FORMAT_DATETIME,
-                       FORMAT_DATETIME.rstrip('Z'),
-                       FORMAT_DATETIME.rstrip(':%SZ'),
-                       FORMAT_DATE,
-                       FORMAT_DATE.rstrip('-%d'),
-                       FORMAT_TIME,
-                       FORMAT_TIME.rstrip(':%S'))
 
-    VALUES_TRUE = ('t', 'true', 'on', '1')
-    VALUES_FALSE = ('f', 'false', 'off', '0')
+    VALUES_TRUE = ('t', 'true', 'on', '1', 'yes')
+    VALUES_FALSE = ('f', 'false', 'off', '0', 'no')
     VALUES_NULL = ('null', 'none', 'na')
 
     registry = dict()
@@ -30,6 +29,7 @@ class DataType:
         self.ma_type = self.get_ma_type(ma_type)
         self.sa_types = tuple(self.get_sa_type(sa_type) for sa_type in sa_types)
         self.parser = kwargs.get('parser', str)
+        self.accept = kwargs.get('accept', tuple())
         operators, operators_multi = kwargs.get(
             'filter', ((Operator.NONE, Operator.EQ, Operator.NE),
                        (Operator.NONE, Operator.EQ, Operator.NE)))
@@ -55,19 +55,6 @@ class DataType:
         DataType.registry[sa_type] = self
         return sa_type
 
-    @classmethod
-    def get_datetime_format(cls, val):
-        for fmt in cls.DATETIME_ACCEPT:
-            length = len(val) - 2 if '-' in val else len(val)
-            if len(fmt) == length:
-                if len(fmt) > 8:
-                    return fmt
-                if '-' in fmt and '-' in val:
-                    return fmt
-                if ':' in fmt and ':' in val:
-                    return fmt
-        return cls.FORMAT_DATETIME
-
     def parse(self, val):
         if val.lower() in DataType.VALUES_NULL:
             return
@@ -90,13 +77,44 @@ class DataType:
 
 
 def parse_bool(val):
+    if not isinstance(val, str):
+        raise ValueError
     if val.lower() not in DataType.VALUES_TRUE and val.lower() not in DataType.VALUES_FALSE:
         raise ValueError
     return val.lower() in DataType.VALUES_TRUE
 
 
+def parse_date(val):
+    if not isinstance(val, str):
+        raise ValueError
+    for fmt in accept_date:
+        try:
+            return dt.datetime.strptime(val, fmt).date()
+        except ValueError:
+            pass
+    raise ValueError
+
+
+def parse_time(val):
+    if not isinstance(val, str):
+        raise ValueError
+    for fmt in accept_time:
+        try:
+            return dt.datetime.strptime(val, fmt).time()
+        except ValueError:
+            pass
+    raise ValueError
+
+
 def parse_datetime(val):
-    return dt.datetime.strptime(val, DataType.get_datetime_format(val))
+    if not isinstance(val, str):
+        raise ValueError
+    for fmt in accept_datetime:
+        try:
+            return dt.datetime.strptime(val, fmt)
+        except ValueError:
+            pass
+    raise ValueError
 
 
 Bool = DataType(
@@ -129,14 +147,14 @@ String = DataType(
 Date = DataType(
     ma.fields.Date,
     sqltypes.Date,
-    parser=parse_datetime,
+    parser=parse_date,
     filter=((Operator.GT, Operator.LT),
             (Operator.NONE, Operator.EQ, Operator.NE)))
 
 Time = DataType(
     ma.fields.Time,
     sqltypes.Time,
-    parser=parse_datetime,
+    parser=parse_time,
     filter=((Operator.GT, Operator.LT),
             (Operator.NONE, Operator.EQ, Operator.NE)))
 
--- jsonapi/tests/test_datatypes.py
+++ jsonapi/tests/test_datatypes.py
@@ -1,10 +1,13 @@
+import datetime as dt
+
 import pytest
 
-from jsonapi.tests.util import *
+from jsonapi.datatypes import parse_bool, parse_date, parse_datetime, parse_time
+from jsonapi.tests.util import assert_datatype, is_date, is_datetime, is_time
 
 
 #
-# public access (no login)
+# test data
 #
 
 
@@ -91,3 +94,120 @@ async def test_json(test_data):
 @pytest.mark.asyncio
 async def test_json_b(test_data):
     assert_datatype(test_data, 'testJsonB', lambda v: isinstance(v, dict))
+
+
+#
+# parsers
+#
+
+
+@pytest.mark.asyncio
+async def test_parse_bool():
+    for val in ('t', 'T', 'true', 'True', 'TRUE', '1', 'on', 'On', 'ON', 'yes', 'Yes', 'YES'):
+        assert parse_bool(val) is True
+    for val in ('f', 'F', 'false', 'False', 'FALSE', '0', 'off', 'Off', 'OFF', 'no', 'No', 'NO'):
+        assert parse_bool(val) is False
+
+
+@pytest.mark.asyncio
+async def test_parse_bool_error():
+    for val in ('tt', 'FF', False, True, 0, 1, 2):
+        try:
+            parse_bool(val)
+        except ValueError:
+            assert True
+        else:
+            assert False
+
+
+test_input = dict(
+    other=('foo', True, 0, 1, 2),
+    date=dict(
+        good=('2019-09-01', '2019-9-1', '2019-09-1', '2019-9-01', '2019-09', '2019-9'),
+        bad=('2019/09/01', '2019-Sep-01', '09-01-19', 'September 1st, 2019', 'Sep 1/19',
+             '2019-09-32', '2019-13-01', '19-09-01')
+    ),
+    time=dict(
+        good=('16:09:00', '16:09', '4:09 PM', '16:09', '4:09 PM', '4:09PM',
+              '16:9:00', '16:9', '4:9 PM', '16:9', '4:9 PM', '4:9PM'),
+        bad=('60:00:00', '14:01:31 PM', '09:91', '16:35 PM')
+    ),
+    datetime=dict(
+        good=('2019-09-01T16:09:00Z',
+              '2019-09-01T16:09:00', '2019-09-01T16:09',
+              '2019-09-01 16:09:00', '2019-09-01 16:09',
+              '2019-09-01 4:09:00PM', '2019-09-01 4:09:00 PM', '2019-09-01 4:09PM', '2019-09-01 4:09 PM',
+              '2019-09-01 4:09:00pm', '2019-09-01 4:09:00 pm', '2019-09-01 4:09pm', '2019-09-01 4:09 pm',
+              '2019-09-01 16:9:00', '2019-09-01 16:9',
+              '2019-09-01 4:9:0PM', '2019-09-01 4:9PM'),
+        bad=('2019/09/01 16:09:00', '2019-Sep-01 16:09:00', '09-01-19 16:09:00',
+             'September 1st, 2019 16:09:00', 'Sep 1/19 16:09pm',
+             '2019-09-32 16:09:00', '2019-13-01 16:09:00', '19-09-01 16:09:00')
+    )
+)
+
+
+@pytest.mark.asyncio
+async def test_parse_date():
+    for val in test_input['date']['good']:
+        assert parse_date(val) == dt.date(2019, 9, 1)
+
+
+@pytest.mark.asyncio
+async def test_parse_date_error():
+    for val in (*test_input['date']['bad'],
+                *test_input['time']['good'],
+                *test_input['time']['bad'],
+                *test_input['datetime']['good'],
+                *test_input['datetime']['bad'],
+                *test_input['other']):
+        try:
+            parse_date(val)
+        except ValueError:
+            assert True
+        else:
+            assert False
+
+
+@pytest.mark.asyncio
+async def test_parse_time():
+    for val in test_input['time']['good']:
+        assert parse_time(val) == dt.time(16, 9)
+
+
+@pytest.mark.asyncio
+async def test_parse_time_error():
+    for val in (*test_input['time']['bad'],
+                *test_input['date']['good'],
+                *test_input['date']['bad'],
+                *test_input['datetime']['good'],
+                *test_input['datetime']['bad'],
+                *test_input['other']):
+        try:
+            parse_time(val)
+        except ValueError:
+            assert True
+        else:
+            assert False
+
+
+@pytest.mark.asyncio
+async def test_parse_datetime():
+    for val in test_input['datetime']['good']:
+        assert parse_datetime(val) == dt.datetime(2019, 9, 1, 16, 9)
+
+
+@pytest.mark.asyncio
+async def test_parse_datetime_error():
+    for val in (*test_input['datetime']['bad'],
+                *test_input['time']['good'],
+                *test_input['time']['bad'],
+                *test_input['date']['good'],
+                *test_input['date']['bad'],
+                *test_input['other']):
+        try:
+            parse_datetime(val)
+        except ValueError:
+            assert True
+        else:
+            assert False
